83ee9d90ea8a677557c1bceb41f9850b0292a3cf
Erildo07 <erildonunes12@gmail.com>
2026-05-13 19:32:52 -0300

    adiciona arquivos aula3 e hora_codar

---
+++ Tema 3 - Super Trunfo/aula3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+  float temperatura = 25.0;
+
+  if (temperatura > 30.0) {
+    printf("Esta calor\n");
+  } else {
+    printf("Nao esta calor\n");
+  }
+
+  return 0;
+}
---
+++ Tema 3 - Super Trunfo/hora_codar.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+
+    float temperatura, umidade;
+    unsigned int estoque, estoqueMinimo = 1000;
+
+    printf("Entre com a temperatura: ");
+    scanf("%f", &temperatura);
+
+    printf("Entre com a umidade: ");
+    scanf("%f", &umidade);
+
+    printf("Entre com o estoque: ");
+    scanf("%u", &estoque);
+
+    if (temperatura > 30) {
+        printf("Temperatura esta alta\n");
+    } else {
+        printf("Temperatura esta dentro dos parametros\n");
+    }
+
+    if (umidade > 50) {
+        printf("Umidade elevada\n");
+    } else {
+        printf("Umidade dentro dos parametros\n");
+    }
+
+    if (estoque < estoqueMinimo) {
+        printf("Estoque abaixo do minimo\n");
+    } else {
+        printf("Estoque normal\n");
+    }
+
+    return 0;
+}
