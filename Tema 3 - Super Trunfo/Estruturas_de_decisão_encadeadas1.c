4b9917a5277bf0ba79e7fceb8b0213e664b38a54
Erildo07 <erildonunes12@gmail.com>
2026-05-17 19:08:49 -0300

    estrutura encadeada1

---
+++ Tema 3 - Super Trunfo/Estruturas_de_decisão_encadeadas1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+
+    int nota;
+
+    printf("Digite a sua nota: ");
+    scanf("%d", &nota);
+
+    // A >= 90
+    // B >= 80
+    // C >= 70
+    // D >= 60
+    // F < 60
+
+
+    if ()
