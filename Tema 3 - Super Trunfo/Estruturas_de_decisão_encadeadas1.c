622c757678759ab2994573d70dbbf5b4143c739a
Erildo07 <erildonunes12@gmail.com>
2026-05-18 21:28:11 -0300

    comentario

--- Tema 3 - Super Trunfo/Estruturas_de_decisão_encadeadas1.c
+++ Tema 3 - Super Trunfo/Estruturas_de_decisão_encadeadas1.c
@@ -2,9 +2,13 @@
 
 int main() {
 
+    // Variável que guarda a nota
     int nota;
 
+    // Pede a nota ao usuário
     printf("Digite a sua nota: ");
+
+    // Lê a nota digitada
     scanf("%d", &nota);
 
     // A >= 90
@@ -13,22 +17,32 @@ int main() {
     // D >= 60
     // F < 60
 
+    // Verifica se a nota é maior ou igual a 90
+    if (nota >= 90) {
 
-    if (nota >= 90){
         printf("Conceito A!");
 
-    }  else if (nota >= 80) {
-        printf("Conceito e B:");
+    // Verifica se a nota é maior ou igual a 80
+    } else if (nota >= 80) {
 
+        printf("Conceito B!");
+
+    // Verifica se a nota é maior ou igual a 70
     } else if (nota >= 70) {
-        printf("Conceito e C: ");
 
+        printf("Conceito C!");
+
+    // Verifica se a nota é maior ou igual a 60
     } else if (nota >= 60) {
-        printf("Conceito e D");
 
+        printf("Conceito D!");
+
+    // Se não entrar em nenhuma condição acima
     } else {
-        printf("Conceito e F:");
+
+        printf("Conceito F!");
     }
-    
+
+    // Finaliza o programa
     return 0;
 }
